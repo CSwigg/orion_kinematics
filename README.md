8ed2f8f9c30d3e1f93c9e364338809c611438a8a
CSwigg <swiggum2@wisc.edu>
2020-04-11 19:31:59 -0500

    first

---
+++ README.md
@@ -0,0 +1,5 @@
+# Purpose
+Routines built utilizing astropy coordinate frames and conversions 
+that put a stellar group in its own reference frame by subtracting
+off LSR motion and the average motion of the group. Plotting routines 
+included.
---
+++ plotting.py
@@ -0,0 +1,70 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D  
+from astropy.coordinates import (CartesianRepresentation,CartesianDifferential,SphericalRepresentation,SphericalDifferential)
+from astropy.coordinates import ICRS, Galactic, GalacticLSR, Galactocentric
+
+def cartesian_3d(cf):
+    '''
+    Plots 3d cartesian coordinate frame
+    '''
+    assert cf.representation_type == CartesianRepresentation
+    assert cf.differential_type == CartesianDifferential
+    x = cf.x.value
+    y = cf.y.value
+    z = cf.z.value
+
+    v_x = cf.v_x.value
+    v_y = cf.v_y.value
+    v_z = cf.v_z.value
+
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    ax.quiver(x,y,z,v_x,v_y,v_z,normalize=True,length=10,linewidth=.5)
+    ax.set_xlabel('x (pc)')
+    ax.set_ylabel('y (pc)')
+    ax.set_zlabel('z (pc)')
+    plt.show()
+
+def equitorial_3d(cf):
+    '''
+    Plots 3d equitorial coordinate frame
+    '''
+    #assert cf.representation_type == SphericalRepresentation
+    #assert cf.differential_type == SphericalDifferential
+
+    ra = cf.ra.value
+    dec = cf.dec.value
+    d = cf.distance.value
+
+    pmra = cf.pm_ra_cosdec.value
+    pmdec = cf.pm_dec.value
+    rv = cf.radial_velocity.value
+
+    fig = plt.figure()
+    ax = fig.add_subplot(projection = '3d')
+    ax.quiver(ra,dec,d,pmra,pmdec,rv,normalize=True)
+    ax.set_xlabel('ra (deg)')
+    ax.set_ylabel('dec (deg)')
+    ax.set_zlabel('Distance (deg)')
+    plt.show() 
+
+def equitorial_2d(cf):
+    '''
+    Plots 2d equitorial
+    '''
+    #assert cf.representation_type == SphericalRepresentation
+    #assert cf.differential_type == SphericalDifferential
+
+    ra = cf.ra.value
+    dec = cf.dec.value
+    
+    pmra = cf.pm_ra_cosdec.value
+    pmdec = cf.pm_dec.value
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.quiver(ra,dec,-pmra,pmdec,color='blue')
+    ax.set_xlabel('ra (deg)')
+    ax.set_ylabel('dec (deg)')
+    plt.gca().invert_xaxis()
+    plt.show()
